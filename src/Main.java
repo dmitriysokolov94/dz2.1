1b4876e253478c01abb8e662d90646379a2c9e21
dmitriysokolov94 <ipsokolov2021@gmail.com>
2022-04-30 11:53:58 +0300

    mew project

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 8000;
+        int bonus = 20;
+        int total = price / bonus;
+        System.out.println(total);
+    }
+}
